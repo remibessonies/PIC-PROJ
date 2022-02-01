c963ba34929870bb7045491d16240ac9ed1ef996
Yanan <51014935+CYNAchille@users.noreply.github.com>
2022-02-01 15:25:54 +0100

    Update train.py

--- training/train.py
+++ training/train.py
@@ -2,7 +2,7 @@ from transformers import LayoutLMForTokenClassification
 import torch
 from transformers import AdamW
 from tqdm import tqdm
-from datasets.funsd import train_dataloader,eval_dataloader
+from dataloading.funsd import train_dataloader,eval_dataloader
 
 def get_labels(path):
     with open(path, "r") as f:
