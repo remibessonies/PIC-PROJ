c2ed4546c644b7205e69de84074e08b1685a1b74
Yoldoz Tabei <78078014+yoldoz@users.noreply.github.com>
2022-02-01 14:37:39 +0100

    Update train.py

--- training/train.py
+++ training/train.py
@@ -1,15 +1,15 @@
 pretrained_model_folder_input= sroie_folder_path / Path('layoutlm-base-uncased') # Define it so we can copy it into our working directory
 
-pretrained_model_folder=Path('/kaggle/working/layoutlm-base-uncased/') 
+pretrained_model_folder=Path('/output/working/layoutlm-base-uncased/') 
 label_file=Path(dataset_directory, "labels.txt")
 
 # Move to the script directory
-os.chdir("/kaggle/working/unilm/layoutlm/deprecated/examples/seq_labeling")
+os.chdir("/output/working/unilm/layoutlm/deprecated/examples/seq_labeling")
 
 ! cp -r "{pretrained_model_folder_input}" "{pretrained_model_folder}"
 ! sed -i 's/"num_attention_heads": 16,/"num_attention_heads": 12,/' "{pretrained_model_folder}/"config.json
 
-! cat "/kaggle/working/layoutlm-base-uncased/config.json"
+! cat "/output/working/layoutlm-base-uncased/config.json"
 
 ! rm -rf /kaggle/working/dataset/cached*
 
@@ -28,4 +28,3 @@ os.chdir("/kaggle/working/unilm/layoutlm/deprecated/examples/seq_labeling")
                             --overwrite_output_dir \
                             --per_gpu_train_batch_size 8 \
                             --per_gpu_eval_batch_size 16
-
