f8789af42a74845a8e205f2f37c94f797cd1e6cb
Yanan <51014935+CYNAchille@users.noreply.github.com>
2022-02-08 15:22:38 +0100

    Update ancient_sroie.py

--- data_loading/ancient_sroie.py
+++ data_loading/ancient_sroie.py
@@ -4,6 +4,24 @@ import shutil
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 
+config = {
+    "tokenizer_name": "microsoft/layoutlm-base-uncased",
+    "model_name": "microsoft/layoutlm-base-uncased",
+    "data_raw_path": "SROIE",
+    "data_path": "data",
+    "n_samples": None,  # None for full dataset
+    "model_path": "model",
+    "num_labels": 5,
+    "pct_train": 0.8,
+    "learning_rate": 0.001,
+    "batch_size_train": 16,
+    "batch_size_test": 1,
+    "epochs": 100,
+    "log_freq": 2,
+}
+
+task_1_dir = "0325updated.task1train(626p)"
+
 class SROIE(Dataset):
     def __init__(
         self,
