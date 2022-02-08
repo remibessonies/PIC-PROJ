2e3b729fecf73680add32ba32aaf2952d6c5ce07
Yanan <51014935+CYNAchille@users.noreply.github.com>
2022-02-08 16:01:03 +0100

    Update utils.py

--- data_loading/utils.py
+++ data_loading/utils.py
@@ -2,15 +2,15 @@ from sroie.py import dataloader_train_sroie, dataloader_test_sroie
 from funsd.py import train_dataloader, eval_dataloader
 
 def dataloader_train(Dataset):
-  if Dataset=='SROIE':
+  if Dataset=='sroie':
     return(dataloader_train_sroie)
-  elif Dataset=='FUNSD':
+  elif Dataset=='funsd':
     return(train_dataloader)
   
 def dataloader_test(Dataset):
-  if Dataset=='SROIE':
+  if Dataset=='sroie':
     return(dataloader_test_sroie)
-  elif Dataset=='FUNSD':
+  elif Dataset=='funsd':
     return(eval_dataloader)
   
 
